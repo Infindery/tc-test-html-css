f3e04525306f43eee6dbd1ad2fc6db7c141c7219
Artem <sheptunov-aa@mail.ru>
2024-04-14 17:48:43 +0500

    Creating a test for the updateFrameAndTotalScore method

--- js/tests/main.test.js
+++ js/tests/main.test.js
@@ -25,3 +25,18 @@ test("Recording the result in gameScoreTable", () => {
 	expect(game.gameScoreTable.size).toBe(1);
 	expect(game.gameScoreTable.get(1).result).toEqual([5]);
 });
+
+test("Update frameScore and totalScore in frames with strike/spear", () => {
+	const game = new Game();
+
+	expect(game.recordingResultToFrame(1, 5)).toBe(0);
+	expect(game.recordingResultToFrame(2, 2)).toBe(0);
+	expect(game.recordingResultToFrame(3, 1)).toBe(0);
+	expect(game.updateFrameAndTotalScore(3)).toBe(0);
+	expect(game.gameScoreTable.get(1).frameScore).toBe(8);
+	expect(game.gameScoreTable.get(2).frameScore).toBe(5);
+	expect(game.gameScoreTable.get(3).frameScore).toBe(4);
+	expect(game.gameScoreTable.get(1).totalScore).toBe(8);
+	expect(game.gameScoreTable.get(2).totalScore).toBe(13);
+	expect(game.gameScoreTable.get(3).totalScore).toBe(17);
+});
