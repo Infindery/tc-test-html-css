020aeef03840ad143c9b9c7ee3ffc3da646c4046
Artem <sheptunov-aa@mail.ru>
2024-04-14 17:00:16 +0500

    Defining the getPreviousFrameResult Method

--- ts/main.ts
+++ ts/main.ts
@@ -19,6 +19,14 @@ class Game {
 
         return this.gameScoreTable.get(this.gameScoreTable.size).totalScore;
     }
+
+	public getPreviousFrameResult() {
+		if (!this.gameScoreTable.has(this.gameScoreTable.size - 1)) {
+			return -1;
+		}
+
+		return this.gameScoreTable.get(this.gameScoreTable.size - 1).result;
+	}
 }
 
 module.exports = Game;
