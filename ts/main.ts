e3662c87cd779d993ad9bdd2836135a11bbed112
Artem <sheptunov-aa@mail.ru>
2024-04-14 16:50:19 +0500

    Defining the getCurrentTotalScore method and the frameObject interface

--- ts/main.ts
+++ ts/main.ts
@@ -1,8 +1,23 @@
+interface farmeObject {
+	frame: number,
+	result: number[] | string[],
+	frameScore: number,
+	totalScore: number
+}
+
 class Game {
-	private gameScoreTable: Map<number, object>;
+	private gameScoreTable: Map<number, farmeObject>;
 
 	constructor() {
         this.gameScoreTable = new Map();
+    }
+
+	getCurrentTotalScore() {
+		if (this.gameScoreTable.size <= 0) {
+			return 0;
+		}
+
+        return this.gameScoreTable.get(this.gameScoreTable.size).totalScore;
     }
 }
 
