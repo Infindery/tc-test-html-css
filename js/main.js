3e919a16c8ee41c759458675aa35a146f1cc8805
Artem <sheptunov-aa@mail.ru>
2024-04-14 16:56:22 +0500

    Сreating a test for getting a previous result

--- js/main.js
+++ js/main.js
@@ -3,5 +3,11 @@ class Game {
     constructor() {
         this.gameScoreTable = new Map();
     }
+    getCurrentTotalScore() {
+        if (this.gameScoreTable.size <= 0) {
+            return 0;
+        }
+        return this.gameScoreTable.get(this.gameScoreTable.size).totalScore;
+    }
 }
 module.exports = Game;
--- js/tests/main.test.js
+++ js/tests/main.test.js
@@ -1,7 +1,13 @@
-const Game = require("../js/main");
+const Game = require("../main");
 
 test("Getting a current totalScore with an empty table", () => {
 	const game = new Game();
 
 	expect(game.getCurrentTotalScore()).toBe(0);
-})
+});
+
+test("Getting a previous result with an empty table", () => {
+	const game = new Game();
+
+	expect(game.getPreviousFrameResult()).toBe(-1);
+});
