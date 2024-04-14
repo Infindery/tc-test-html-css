6f5de9b745cd5a578042d1313d90402832e1640d
Artem <sheptunov-aa@mail.ru>
2024-04-14 18:02:33 +0500

    Creating a test for the throwDistribution method

--- js/tests/main.test.js
+++ js/tests/main.test.js
@@ -40,3 +40,22 @@ test("Update frameScore and totalScore in frames with strike/spear", () => {
 	expect(game.gameScoreTable.get(2).totalScore).toBe(13);
 	expect(game.gameScoreTable.get(3).totalScore).toBe(17);
 });
+
+test("Adding throws to the table", () => {
+	const game = new Game();
+
+	expect(game.throwDistribution(10)).toBe(0);
+	expect(game.throwDistribution(5)).toBe(0);
+	expect(game.throwDistribution(5)).toBe(0);
+	expect(game.throwDistribution(2)).toBe(0);
+	expect(game.throwDistribution(1)).toBe(0);
+	expect(game.gameScoreTable.get(1).result).toEqual(["X"]);
+	expect(game.gameScoreTable.get(2).result).toEqual([5, "/"]);
+	expect(game.gameScoreTable.get(3).result).toEqual([2, 1]);
+	expect(game.gameScoreTable.get(1).frameScore).toBe(20);
+	expect(game.gameScoreTable.get(1).frameScore).toBe(12);
+	expect(game.gameScoreTable.get(1).frameScore).toBe(3);
+	expect(game.gameScoreTable.get(1).totalScore).toBe(20);
+	expect(game.gameScoreTable.get(2).totalScore).toBe(32);
+	expect(game.gameScoreTable.get(2).totalScore).toBe(35);
+});
